aff24e2796f454000a5b967fa658687bb0700ac3
Booknerd2000 <denis.ajuah.2021@student.lhschools.org>
2019-05-21 17:33:02 -0400

    Created random cpuChakra generator

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,12 +5,21 @@ public class Main {
     public static void main(String[] args) {
         Scanner keyboard;
         keyboard = new Scanner(System.in);
-        
+
+        int cpu;
+        String cpuChakra;
+
         String[] chakra = new String[5];
         chakra[0] = "fire";
         chakra[1] = "wind";
         chakra[2] = "lightning";
         chakra[3] = "earth";
         chakra[4] = "water";
+
+        cpu = (int) (Math.random() * 5);
+        cpuChakra = chakra[cpu];
+
+        System.out.println(cpuChakra);
+
     }
 }
