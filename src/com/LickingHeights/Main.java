e7a185938430911576da2b81028749454f5dc143
Booknerd2000 <denis.ajuah.2021@student.lhschools.org>
2019-05-21 17:25:52 -0400

    created user input

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,11 @@
 package com.LickingHeights;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
+
+        
     }
 }
