ff4555e694236f02ccda28e1eb09c84d74b4170b
Booknerd2000 <denis.ajuah.2021@student.lhschools.org>
2019-05-21 19:17:42 -0400

    Put switch statement into 2 different method

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -30,60 +30,81 @@ public class Main {
             userChakra = chakra[user];
             System.out.println(userChakra);
 
+            cpuWin(cpuChakra,userChakra,cpuPoints);
+            cpuLose(userChakra,cpuChakra,userPoints);
+            
 
-            switch(cpuChakra){
-                case "fire":
-                    if(userChakra == "wind" || userChakra == "lightning") {
-                        cpuPoints++;
-                    }else if (userChakra == cpuChakra) {
-                        System.out.println("tie");
-                    } else {
-                        userPoints++;
-                    }
-                    break;
-
-                case "wind":
-                    if (userChakra == "lightning" || userChakra == "earth") {
-                        cpuPoints++;
-                    }else if (userChakra == cpuChakra) {
-                        System.out.println("tie");
-                    } else {
-                        userPoints++;
-                    }
-                    break;
-
-                case "lightning":
-                    if (userChakra == "earth" || userChakra == "water") {
-                        cpuPoints++;
-                    }else if (userChakra == cpuChakra) {
-                        System.out.println("tie");
-                    } else {
-                        userPoints++;
-                    }
-                    break;
-
-                case "earth":
-                    if (userChakra == "water" || userChakra == "fire") {
-                        cpuPoints++;
-                    }else if (userChakra == cpuChakra) {
-                        System.out.println("tie");
-                    } else {
-                        userPoints++;
-                    }
-                    break;
-
-                case "water":
-                    if (userChakra == "fire" || userChakra == "wind") {
-                        cpuPoints++;
-                    }else if (userChakra == cpuChakra) {
-                        System.out.println("tie");
-                    } else {
-                        userPoints++;
-                    }
-                    break;
-            }
             System.out.println("cpuPoint"+cpuPoints+"userPoints"+cpuPoints);
         }
 
+    }
+    public static int cpuWin(String cpuChakra,String userChakra,int cpuPoints){
+
+        switch(cpuChakra){
+            case "fire":
+                if(userChakra == "wind" || userChakra == "lightning") {
+                    cpuPoints++;
+                }
+                break;
+
+            case "wind":
+                if (userChakra == "lightning" || userChakra == "earth") {
+                    cpuPoints++;
+                }
+                break;
+
+            case "lightning":
+                if (userChakra == "earth" || userChakra == "water") {
+                    cpuPoints++;
+                }
+                break;
+
+            case "earth":
+                if (userChakra == "water" || userChakra == "fire") {
+                    cpuPoints++;
+                }
+                break;
+
+            case "water":
+                if (userChakra == "fire" || userChakra == "wind") {
+                    cpuPoints++;
+                }
+                break;
+        }
+        return cpuPoints;
+    }
+    public static int cpuLose(String userChakra,String cpuChakra,int userPoints){
+        switch(userChakra){
+            case "fire":
+                if(cpuChakra == "wind" || cpuChakra == "lightning") {
+                    userPoints++;
+                }
+                break;
+
+            case "wind":
+                if (cpuChakra == "lightning" || cpuChakra == "earth") {
+                    userPoints++;
+                }
+                break;
+
+            case "lightning":
+                if (cpuChakra == "earth" || cpuChakra == "water") {
+                    userPoints++;
+                }
+                break;
+
+            case "earth":
+                if (cpuChakra == "water" || cpuChakra == "fire") {
+                    userPoints++;
+                }
+                break;
+
+            case "water":
+                if (cpuChakra == "fire" || cpuChakra == "wind") {
+                    userPoints++;
+                }
+                break;
+        }
+        return userPoints;
     }
 }
