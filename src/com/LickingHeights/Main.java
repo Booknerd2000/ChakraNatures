4b602d8036a99110c10af3c3584f45ce31c2e5df
Booknerd2000 <denis.ajuah.2021@student.lhschools.org>
2019-05-21 17:28:11 -0400

    Created and array to store chakra natures

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -5,7 +5,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner keyboard;
         keyboard = new Scanner(System.in);
-
         
+        String[] chakra = new String[5];
+        chakra[0] = "fire";
+        chakra[1] = "wind";
+        chakra[2] = "lightning";
+        chakra[3] = "earth";
+        chakra[4] = "water";
     }
 }
