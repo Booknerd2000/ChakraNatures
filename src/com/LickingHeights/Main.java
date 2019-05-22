2e2ae979b6771927fa850aa359df3027927fe9ca
Booknerd2000 <denis.ajuah.2021@student.lhschools.org>
2019-05-21 21:56:07 -0400

    re assign new names to the code and reorganize it. Also added chakra

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -16,23 +16,37 @@ public class Main {
         boolean mainLoop=true;
 
         String[] chakra = new String[5];
-        chakra[0] = "fire";
-        chakra[1] = "wind";
-        chakra[2] = "lightning";
-        chakra[3] = "earth";
-        chakra[4] = "water";
+        chakra[0] = "Fire";
+        chakra[1] = "Wind";
+        chakra[2] = "Lightning";
+        chakra[3] = "Earth";
+        chakra[4] = "Water";
 
-        System.out.println("Fire = 0 \nWind = 1 \nLightning = 2\nEarth = 3\nWater = 4\n");
+        System.out.println("\033[4m" +"\033[1;36m"+"Chakra Natures"+"\033[49m");
+        System.out.println("\n" +
+                "                 ......             .                                              \n"+
+                "     .          ..   ...           . .                                             \n" +
+                "    ..              ....         .           ..........           .....     .....  \n" +
+                "   ... .   ............         . .        ..............        .      .  .      .\n" +
+                " ....  ..     ..............  .    .      ....    ........      ..   ...  ..   ... \n" +
+                "... ......                ...     . .     .........  .....     ...       ...       \n" +
+                ".... .....            ..   ..   ..   .     ...  ... .....     ....... .......      \n" +
+                "  ......               .....   .            ............    ..................     ");
+
+        System.out.println("   Fire          Wind          Lightning        Earth              Water");
+        System.out.println("    0             1                2              3                  4\n");
+        System.out.println("\033[4m" +"Pick a Chakra"+"\033[49m");
 
         while(mainLoop){
             while(gameLoop){
+
                 cpu = (int) (Math.random() * 5);
                 cpuChakra = chakra[cpu];
-                System.out.println(cpuChakra);
+
 
                 user = keyboard.nextInt();
                 userChakra = chakra[user];
-                System.out.println(userChakra);
+                System.out.println("\033[0;30m"+userChakra+"  V.S  "+cpuChakra+"\033[49m");
 
                 cpuPoints= cpuWin(cpuChakra,userChakra,cpuPoints);
                 //System.out.println(cpuPoints);
@@ -42,13 +56,18 @@ public class Main {
                     System.out.println("Draw");
                 }
 
-                System.out.println("cpuPoint "+cpuPoints+" userPoints "+userPoints);
+                System.out.println("\033[0;30m"+"You    CPU\n " +userPoints+"      "+cpuPoints+"\033[49m");
 
                 if (userPoints==5 || cpuPoints==5){
                     gameLoop = false;
                 }
             }
-            System.out.println("Want to play again\n[yes=1] [no=2]");
+            if (userPoints==5){
+                System.out.println("\033[0;33m"+"YOU WIN"+"\033[49m");
+            } else if(cpuPoints==5){
+                System.out.println("\033[0;31m"+"YOU LOSE"+"\033[49m");
+            }
+            System.out.println("\033[1;34m"+"Want to play again?\n[yes=1] or [no=2]"+"\033[49m");
             restart=keyboard.nextInt();
             if(restart ==1){
                 userPoints = 0;
@@ -63,38 +82,38 @@ public class Main {
     public static int cpuWin(String cpuChakra,String userChakra,int cpuPoints){
 
         switch(cpuChakra){
-            case "fire":
-                if(userChakra == "wind" || userChakra == "lightning") {
+            case "Fire":
+                if(userChakra == "Wind" || userChakra == "Lightning") {
                     cpuPoints++;
-                    System.out.println("Defeat");
+                    System.out.println("\033[0;31m"+"Defeat"+"\033[49m");
                 }
                 break;
 
-            case "wind":
-                if (userChakra == "lightning" || userChakra == "earth") {
+            case "Wind":
+                if (userChakra == "Lightning" || userChakra == "Earth") {
                     cpuPoints++;
-                    System.out.println("Defeat");
+                    System.out.println("\033[0;31m"+"Defeat"+"\033[49m");
                 }
                 break;
 
-            case "lightning":
-                if (userChakra == "earth" || userChakra == "water") {
+            case "Lightning":
+                if (userChakra == "Earth" || userChakra == "Water") {
                     cpuPoints++;
-                    System.out.println("Defeat");
+                    System.out.println("\033[0;31m"+"Defeat"+"\033[49m");
                 }
                 break;
 
-            case "earth":
-                if (userChakra == "water" || userChakra == "fire") {
+            case "Earth":
+                if (userChakra == "Water" || userChakra == "Fire") {
                     cpuPoints++;
-                    System.out.println("Defeat");
+                    System.out.println("\033[0;31m"+"Defeat"+"\033[49m");
                 }
                 break;
 
-            case "water":
-                if (userChakra == "fire" || userChakra == "wind") {
+            case "Water":
+                if (userChakra == "Fire" || userChakra == "Wind") {
                     cpuPoints++;
-                    System.out.println("Defeat");
+                    System.out.println("\033[0;31m"+"Defeat"+"\033[49m");
                 }
                 break;
         }
@@ -102,38 +121,38 @@ public class Main {
     }
     public static int cpuLose(String userChakra,String cpuChakra,int userPoints){
         switch(userChakra){
-            case "fire":
-                if(cpuChakra == "wind" || cpuChakra == "lightning") {
+            case "Fire":
+                if(cpuChakra == "Wind" || cpuChakra == "Lightning") {
                     userPoints++;
-                    System.out.println("Victory");
+                    System.out.println("\033[1;36m"+"Victory"+"\033[49m");
                 }
                 break;
 
-            case "wind":
-                if (cpuChakra == "lightning" || cpuChakra == "earth") {
+            case "Wind":
+                if (cpuChakra == "Lightning" || cpuChakra == "Earth") {
                     userPoints++;
-                    System.out.println("Victory");
+                    System.out.println("\033[1;36m"+"Victory"+"\033[49m");
                 }
                 break;
 
-            case "lightning":
-                if (cpuChakra == "earth" || cpuChakra == "water") {
+            case "Lightning":
+                if (cpuChakra == "Earth" || cpuChakra == "Water") {
                     userPoints++;
-                    System.out.println("Victory");
+                    System.out.println("\033[1;36m"+"Victory"+"\033[49m");
                 }
                 break;
 
-            case "earth":
-                if (cpuChakra == "water" || cpuChakra == "fire") {
+            case "Earth":
+                if (cpuChakra == "Water" || cpuChakra == "Fire") {
                     userPoints++;
-                    System.out.println("Victory");
+                    System.out.println("\033[1;36m"+"Victory"+"\033[49m");
                 }
                 break;
 
-            case "water":
-                if (cpuChakra == "fire" || cpuChakra == "wind") {
+            case "Water":
+                if (cpuChakra == "Fire" || cpuChakra == "Wind") {
                     userPoints++;
-                    System.out.println("Victory");
+                    System.out.println("\033[1;36m"+"Victory"+"\033[49m");
                 }
                 break;
         }
